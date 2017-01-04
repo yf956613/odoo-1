62621e88b441e3b130a51f4ead14cc92046b68f7
Cocographique <nmi@odoo.com>
2017-01-04 14:37:20 +0100

    [ADD] Salary package Configurator - First mockup

---
+++ addons/hr_salary_package_configurator/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Salary Package Configurator',
+    'version': '1',
+    'category': 'HR',
+    'summary': 'Allow employees to manage their salary package - Front-End',
+    'depends': ['website_portal'],
+    'data': [
+        'views/salary_package_configurator_templates.xml',
+    ],
+    'application': True,
+}
