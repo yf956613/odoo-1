4271967fc297591e65a8ceeb6dae3edff40f0439
Christophe Monniez <moc@odoo.com>
2017-08-31 10:49:39 +0200

    [FIX] http: force read __manifest__ as utf-8

--- odoo/http.py
+++ odoo/http.py
@@ -1311,7 +1311,7 @@ class Root(object):
                     manifest_path = module_manifest(mod_path)
                     path_static = opj(addons_path, module, 'static')
                     if manifest_path and os.path.isdir(path_static):
-                        manifest = ast.literal_eval(open(manifest_path).read())
+                        manifest = ast.literal_eval(open(manifest_path, encoding='utf-8').read())
                         if not manifest.get('installable', True):
                             continue
                         manifest['addons_path'] = addons_path
