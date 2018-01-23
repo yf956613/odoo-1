aa5963729a393b9fa77451062c3da4b4705569b5
Ravi Gohil <rgo@odoo.com>
2018-01-23 15:32:08 +0530

    experiment

--- addons/website_sale/tests/test_sale_process.py
+++ addons/website_sale/tests/test_sale_process.py
@@ -9,9 +9,19 @@ class TestUi(odoo.tests.HttpCase):
         self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop.ready", login="admin")
 
     def test_02_admin_checkout(self):
+#         fp = self.env.ref('account_taxcloud.account_fiscal_position_taxcloud_us', False)
+#         if fp:
+#             print('\n\n\n\n\n\n BEFORE admin checkout', fp.auto_apply)
+#             fp.write({'auto_apply': False})
+#             print('\n\n\n\n\n\n', fp.auto_apply)
         self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product.ready", login="admin")
 
     def test_03_demo_checkout(self):
+#         fp = self.env.ref('account_taxcloud.account_fiscal_position_taxcloud_us', False)
+#         if fp:
+#             print('\n\n\n\n\n\n BEFORE demo checkout', fp.auto_apply)
+#             fp.write({'auto_apply': False})
+#             print('\n\n\n\n\n\n', fp.auto_apply)
         self.phantom_js("/", "odoo.__DEBUG__.services['web_tour.tour'].run('shop_buy_product')", "odoo.__DEBUG__.services['web_tour.tour'].tours.shop_buy_product.ready", login="demo")
 
     # TO DO - add public test with new address when convert to web.tour format.
