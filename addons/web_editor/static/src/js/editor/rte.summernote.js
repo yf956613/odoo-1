0ee33f6c34ddaebc36b183d44e30b78ec035f733
Cocographique <nmi@odoo.com>
2018-06-28 18:01:43 +0200

    [ADD] web_editor: <small> style option

--- addons/web_editor/static/src/js/editor/rte.summernote.js
+++ addons/web_editor/static/src/js/editor/rte.summernote.js
@@ -973,6 +973,7 @@ $.summernote.lang.odoo = {
       normal: _t('Normal'),
       blockquote: _t('Quote'),
       pre: _t('Code'),
+      small: _t('Small'),
       h1: _t('Header 1'),
       h2: _t('Header 2'),
       h3: _t('Header 3'),
