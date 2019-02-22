ae383a57721a6974a70cf013f5ceed009ce26840
Pierre Rousseau <pro@odoo.com>
2019-02-22 11:21:15 +0100

    [IMP] base_vat: remove vatnumber dependency
    Before this commit, two librairies were used to validate VAT numbers: vatnumber and stdnum.
    As stdnum is used in vatnumber and all the specific implementations of vatnumber have been included in stdnum, the vatnumber dependency is removed.
    
    Task-1944107

--- addons/base_vat/models/res_partner.py
+++ addons/base_vat/models/res_partner.py
@@ -6,51 +6,14 @@ import logging
 import string
 import re
 
-_logger = logging.getLogger(__name__)
-try:
-    import vatnumber
-except ImportError:
-    _logger.warning("VAT validation partially unavailable because the `vatnumber` Python library cannot be found. "
-                    "Install it to support more countries, for example with `easy_install vatnumber`.")
-    vatnumber = None
+from stdnum import get_cc_module
+from stdnum.eu.vat import check_vies as stdnum_check_vies
 
 from odoo import api, models, _
 from odoo.tools.misc import ustr
 from odoo.exceptions import ValidationError
-from stdnum.at.uid import compact as compact_at
-from stdnum.be.vat import compact as compact_be
-from stdnum.bg.vat import compact as compact_bg
-from stdnum.ch.vat import compact as compact_ch
-from stdnum.cy.vat import compact as compact_cy
-from stdnum.cz.dic import compact as compact_cz
-from stdnum.de.vat import compact as compact_de
-from stdnum.ee.kmkr import compact as compact_ee
-# el not in stdnum
-from stdnum.es.nif import compact as compact_es
-from stdnum.fi.alv import compact as compact_fi
-from stdnum.fr.tva import compact as compact_fr
-from stdnum.gb.vat import compact as compact_gb
-from stdnum.gr.vat import compact as compact_gr
-from stdnum.hu.anum import compact as compact_hu
-from stdnum.hr.oib import compact as compact_hr
-from stdnum.ie.vat import compact as compact_ie
-from stdnum.it.iva import compact as compact_it
-from stdnum.lt.pvm import compact as compact_lt
-from stdnum.lu.tva import compact as compact_lu
-from stdnum.lv.pvn import compact as compact_lv
-from stdnum.mt.vat import compact as compact_mt
-from stdnum.mx.rfc import compact as compact_mx
-from stdnum.nl.btw import compact as compact_nl
-from stdnum.no.mva import compact as compact_no
-# pe is not in stdnum
-from stdnum.pl.nip import compact as compact_pl
-from stdnum.pt.nif import compact as compact_pt
-from stdnum.ro.cf import compact as compact_ro
-from stdnum.se.vat import compact as compact_se
-from stdnum.si.ddv import compact as compact_si
-from stdnum.sk.dph import compact as compact_sk
-# tr compact vat is not in stdnum
 
+_logger = logging.getLogger(__name__)
 
 _eu_country_vat = {
     'GR': 'EL'
@@ -95,24 +58,26 @@ _ref_vat = {
     'tr': 'TR1234567890 (VERGINO) veya TR12345678901 (TCKIMLIKNO)'  # Levent Karakas @ Eska Yazilim A.S.
 }
 
-
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
+    @api.model
     def _split_vat(self, vat):
         vat_country, vat_number = vat[:2].lower(), vat[2:].replace(' ', '')
         return vat_country, vat_number
 
     @api.model
-    def simple_vat_check(self, country_code, vat_number):
+    def _simple_vat_check(self, country_code, vat_number):
         '''
         Check the VAT number depending of the country.
         http://sima-pc.com/nif.php
         '''
         if not ustr(country_code).encode('utf-8').isalpha():
             return False
-        check_func_name = 'check_vat_' + country_code
-        check_func = getattr(self, check_func_name, None) or getattr(vatnumber, check_func_name, None)
+        check_func = getattr(self, 'check_vat_' + country_code, False)
+        if not check_func:
+            module = get_cc_module(country_code, 'vat')
+            check_func = getattr(module, 'is_valid', False)
         if not check_func:
             # No VAT validation available, default to check that the country code exists
             if country_code.upper() == 'EU':
@@ -124,18 +89,18 @@ class ResPartner(models.Model):
         return check_func(vat_number)
 
     @api.model
-    def vies_vat_check(self, country_code, vat_number):
+    def _vies_vat_check(self, country_code, vat_number):
         try:
             # Validate against  VAT Information Exchange System (VIES)
             # see also http://ec.europa.eu/taxation_customs/vies/
-            return vatnumber.check_vies(country_code.upper() + vat_number)
+            return stdnum_check_vies(country_code.upper() + vat_number)
         except Exception:
             # see http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl
             # Fault code may contain INVALID_INPUT, SERVICE_UNAVAILABLE, MS_UNAVAILABLE,
             # TIMEOUT or SERVER_BUSY. There is no way we can validate the input
             # with VIES if any of these arise, including the first one (it means invalid
             # country code or empty VAT number), so we fall back to the simple check.
-            return self.simple_vat_check(country_code, vat_number)
+            return self._simple_vat_check(country_code, vat_number)
 
     @api.model
     def fix_eu_vat_number(self, country_id, vat):
@@ -158,10 +123,10 @@ class ResPartner(models.Model):
             company = self.env.user.company_id
         if company.vat_check_vies:
             # force full VIES online check
-            check_func = self.vies_vat_check
+            check_func = self._vies_vat_check
         else:
             # quick and partial off-line checksum validation
-            check_func = self.simple_vat_check
+            check_func = self._simple_vat_check
         for partner in self:
             if not partner.vat:
                 continue
@@ -186,71 +151,6 @@ class ResPartner(models.Model):
         if company.vat_check_vies:
             return '\n' + _('The VAT number [%s] for partner [%s] either failed the VIES VAT validation check or did not respect the expected format %s.') % (self.vat, self.name, vat_no)
         return '\n' + _('The VAT number [%s] for partner [%s] does not seem to be valid. \nNote: the expected format is %s') % (self.vat, self.name, vat_no)
-
-    __check_vat_ch_re1 = re.compile(r'(MWST|TVA|IVA)[0-9]{6}$')
-    __check_vat_ch_re2 = re.compile(r'E([0-9]{9}|-[0-9]{3}\.[0-9]{3}\.[0-9]{3})(MWST|TVA|IVA)$')
-
-    def check_vat_ch(self, vat):
-        '''
-        Check Switzerland VAT number.
-        '''
-        # VAT number in Switzerland will change between 2011 and 2013
-        # http://www.estv.admin.ch/mwst/themen/00154/00589/01107/index.html?lang=fr
-        # Old format is "TVA 123456" we will admit the user has to enter ch before the number
-        # Format will becomes such as "CHE-999.999.99C TVA"
-        # Both old and new format will be accepted till end of 2013
-        # Accepted format are: (spaces are ignored)
-        #     CH TVA ######
-        #     CH IVA ######
-        #     CH MWST #######
-        #
-        #     CHE#########MWST
-        #     CHE#########TVA
-        #     CHE#########IVA
-        #     CHE-###.###.### MWST
-        #     CHE-###.###.### TVA
-        #     CHE-###.###.### IVA
-        #
-        if self.__check_vat_ch_re1.match(vat):
-            return True
-        match = self.__check_vat_ch_re2.match(vat)
-        if match:
-            # For new TVA numbers, do a mod11 check
-            num = [s for s in match.group(1) if s.isdigit()]        # get the digits only
-            factor = (5, 4, 3, 2, 7, 6, 5, 4)
-            csum = sum([int(num[i]) * factor[i] for i in range(8)])
-            check = (11 - (csum % 11)) % 11
-            return check == int(num[8])
-        return False
-
-    def _ie_check_char(self, vat):
-        vat = vat.zfill(8)
-        extra = 0
-        if vat[7] not in ' W':
-            if vat[7].isalpha():
-                extra = 9 * (ord(vat[7]) - 64)
-            else:
-                # invalid
-                return -1
-        checksum = extra + sum((8-i) * int(x) for i, x in enumerate(vat[:7]))
-        return 'WABCDEFGHIJKLMNOPQRSTUV'[checksum % 23]
-
-    def check_vat_ie(self, vat):
-        """ Temporary Ireland VAT validation to support the new format
-        introduced in January 2013 in Ireland, until upstream is fixed.
-        TODO: remove when fixed upstream"""
-        if len(vat) not in (8, 9) or not vat[2:7].isdigit():
-            return False
-        if len(vat) == 8:
-            # Normalize pre-2013 numbers: final space or 'W' not significant
-            vat += ' '
-        if vat[:7].isdigit():
-            return vat[7] == self._ie_check_char(vat[:7] + vat[8])
-        elif vat[1] in (string.ascii_uppercase + '+*'):
-            # Deprecated format
-            # See http://www.revenue.ie/en/online/third-party-reporting/reporting-payment-details/faqs.html#section3
-            return vat[7] == self._ie_check_char(vat[2:7] + vat[0] + vat[8])
-        return False
 
     # Mexican VAT verification, contributed by Vauxoo
     # and Panos Christeas <p_christ@hol.gr>
@@ -284,34 +184,6 @@ class ResPartner(models.Model):
         # Valid format and valid date
         return True
 
-    # Norway VAT validation, contributed by Rolv Råen (adEgo) <rora@adego.no>
-    # Support for MVA suffix contributed by Bringsvor Consulting AS (bringsvor@bringsvor.com)
-    def check_vat_no(self, vat):
-        """
-        Check Norway VAT number.See http://www.brreg.no/english/coordination/number.html
-        """
-        if len(vat) == 12 and vat.upper().endswith('MVA'):
-            vat = vat[:-3] # Strictly speaking we should enforce the suffix MVA but...
-
-        if len(vat) != 9:
-            return False
-        try:
-            int(vat)
-        except ValueError:
-            return False
-
-        sum = (3 * int(vat[0])) + (2 * int(vat[1])) + \
-            (7 * int(vat[2])) + (6 * int(vat[3])) + \
-            (5 * int(vat[4])) + (4 * int(vat[5])) + \
-            (3 * int(vat[6])) + (2 * int(vat[7]))
-
-        check = 11 - (sum % 11)
-        if check == 11:
-            check = 0
-        if check == 10:
-            # 10 is not a valid check digit for an organization number
-            return False
-        return check == int(vat[8])
 
     # Peruvian VAT validation, contributed by Vauxoo
     def check_vat_pe(self, vat):
@@ -391,21 +263,11 @@ class ResPartner(models.Model):
 
         return False
 
-    def check_vat_al(self, vat):
-        try:
-            import stdnum.al
-            return stdnum.al.vat.is_valid(vat)
-        except ImportError:
-            return True
-
-    def default_compact(self, vat):
-        return vat
-
     def _fix_vat_number(self, vat):
         vat_country, vat_number = self._split_vat(vat)
-        check_func_name = 'compact_' + vat_country
-        check_func = globals().get(check_func_name) or getattr(self, 'default_compact')
-        vat_number = check_func(vat_number)
+        module = get_cc_module(vat_country, 'vat')
+        if hasattr(module, 'compact'):
+            vat_number = module.compact(vat_number)
         return vat_country.upper() + vat_number
 
     @api.model
