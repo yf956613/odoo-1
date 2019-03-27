64314ae0048f1524892d5ad8ab07c07f81780630
Mohammed Shekha <msh@openerp.com>
2019-03-27 18:33:28 +0530

    [IMP] web: set focus back to editable list field when escape pressed in dialog
    editable o2m, when escape is pressed in dialog which is opened from editable o2m
    field then focus should set back to last active widget in editable listview from
    where dialog is opened, in case of dialog is opened from o2m kanban then closing
    dialog should set focus back to 'Add' button
    
    task-1880688

--- addons/web/static/src/js/fields/relational_fields.js
+++ addons/web/static/src/js/fields/relational_fields.js
@@ -780,6 +780,7 @@ var FieldX2Many = AbstractField.extend({
         discard_changes: '_onDiscardChanges',
         edit_line: '_onEditLine',
         field_changed: '_onFieldChanged',
+        form_dialog_discarded: '_onFormDialogDiscarded',
         open_record: '_onOpenRecord',
         kanban_record_delete: '_onRemoveRecord',
         list_record_remove: '_onRemoveRecord',
@@ -1240,6 +1241,21 @@ var FieldX2Many = AbstractField.extend({
                     ev.data.onFailure();
                 }
             });
+        }
+    },
+    /**
+     * this method is called to set focus back to last active widget when dialog is closed
+     * consider editable o2m opens m2o dialog, when m2o dialog is closed it should set focus
+     * back to m2o field in editable o2m list
+     *
+     * @private
+     */
+    _onFormDialogDiscarded: function (e) {
+        e.stopPropagation();
+        if (this.view.arch.tag === 'kanban') {
+            this.$buttons.find('button.o-kanban-button-new').focus();
+        } else {
+            this.renderer.focusLastActivatedWidget();
         }
     },
     /**
