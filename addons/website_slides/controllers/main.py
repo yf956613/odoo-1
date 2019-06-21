b14737b55048220dfd062cf2b5d0af3dab30390a
Jérémy Hennecart <jeh@odoo.com>
2019-06-21 13:52:46 +0200

    [IMP] eLearning: allow creation of certification on frontend
    Before, you had to create a certification in the backend before adding it
    to a course. Now, when you create a new certification, you can choose an
    existing one or create a new one. If you create a new one, you are redirected
    on the backend to complete the survey after the creation of the slide.
    A member of website_publisher without right on survey can now add a certification
    only if there was already one available. If there is no certification, a simple message
    is displayed to the user.

--- addons/website_slides/controllers/main.py
+++ addons/website_slides/controllers/main.py
@@ -783,7 +783,7 @@ class WebsiteSlides(WebsiteProfile):
     @http.route(['/slides/prepare_preview'], type='json', auth='user', methods=['POST'], website=True)
     def prepare_preview(self, **data):
         Slide = request.env['slide.slide']
-        document_type, document_id = Slide._find_document_data_from_url(data['url'])
+        document_id = Slide._find_document_data_from_url(data['url'])
         preview = {}
         if not document_id:
             preview['error'] = _('Please enter valid youtube or google doc url')
@@ -808,6 +808,34 @@ class WebsiteSlides(WebsiteProfile):
 
         values = dict((fname, post[fname]) for fname in self._get_valid_slide_post_values() if post.get(fname))
 
+        if post.get('category_id'):
+            if post['category_id'][0] == 0:
+                values['category_id'] = request.env['slide.category'].create({
+                    'name': post['category_id'][1]['name'],
+                    'channel_id': values.get('channel_id')}).id
+            else:
+                values['category_id'] = post['category_id'][0]
+
+        new_certification = False
+        if post.get('survey_id'):
+            if post['survey_id'][0] == 0:
+                survey_id = request.env['survey.survey'].create({
+                    'title': post['survey_id'][1]['title'],
+                    'questions_layout': 'page_per_question',
+                    'is_attempts_limited': True,
+                    'attempts_limit': 1,
+                    'is_time_limited': True,
+                    'time_limit': 60.0,
+                    'scoring_type': 'scoring_without_answers',
+                    'certificate': True,
+                    'passing_score': 70.0,
+                    'certification_mail_template_id': request.env['mail.template'].search([('name','=','Certification: Send by email')]).id
+                    })
+                values['survey_id'] = survey_id.id
+                new_certification = True
+            else:
+                values['survey_id'] = post['survey_id'][0]
+                
         # handle exception during creation of slide and sent error notification to the client
         # otherwise client slide create dialog box continue processing even server fail to create a slide
         try:
@@ -859,6 +887,9 @@ class WebsiteSlides(WebsiteProfile):
         if slide.slide_type == "quiz":
             action_id = request.env.ref('website_slides.slide_slide_action').id
             redirect_url = '/web#id=%s&action=%s&model=slide.slide&view_type=form' %( slide.id, action_id)
+        if slide.slide_type == "certification" and new_certification:
+            action_id = request.env.ref('survey.action_survey_form').id
+            redirect_url = '/web#id=%s&action=%s&model=survey.survey&view_type=form' %(survey_id.id,action_id)
         return {
             'url': redirect_url,
             'channel_type': channel.channel_type,
