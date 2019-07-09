ce89501ff584df8d51c7cd00a4625df62662cc7c
Julien Giannone <jgi@odoo.com>
2019-07-09 11:35:30 +0200

    [IMP] livechat: add settings to set livechat colors
    This commit allow users to set up different colors for both
    the live chat button for website visitors and the chat window
    once the live chat button has been clicked. For both, the header
    background color and the text color can be set up.
    
    Task #2030383

--- addons/im_livechat/static/src/js/im_livechat.js
+++ addons/im_livechat/static/src/js/im_livechat.js
@@ -105,6 +105,12 @@ var LivechatButton = Widget.extend({
             }
         }
         this.call('bus_service', 'onNotification', this, this._onNotification);
+        if (this.options.button_background_color) {
+            this.$el.css('background-color', this.options.button_background_color);
+        }
+        if (this.options.button_text_color) {
+            this.$el.css('color', this.options.button_text_color);
+        }
         return this._super();
     },
 
@@ -280,6 +286,8 @@ var LivechatButton = Widget.extend({
         var self = this;
         var options = {
             displayStars: false,
+            headerBackgroundColor: this.options.header_background_color,
+            TitleColor: this.options.title_color,
             placeholder: this.options.input_placeholder || "",
         };
         this._chatWindow = new WebsiteLivechatWindow(this, this._livechat, options);
--- addons/im_livechat/static/tests/website_livechat_window_tests.js
+++ addons/im_livechat/static/tests/website_livechat_window_tests.js
@@ -1,10 +1,9 @@
-odoo.define('mail.websiteLivechatWindowTests', function (require) {
+odoo.define('mail.websitelivechatwindow_test', function (require) {
 "use strict";
 
 var Livechat = require('im_livechat.model.WebsiteLivechat');
 var Message = require('im_livechat.model.WebsiteLivechatMessage');
 var ChatWindow = require('im_livechat.WebsiteLivechatWindow');
-
 var testUtils = require('web.test_utils');
 var Widget = require('web.Widget');
 
@@ -56,6 +55,51 @@ QUnit.test('basic rendering', async function (assert) {
         "should display the correct livechat name and unread message counter");
     assert.containsN(chatWindow, '.o_thread_message', 2,
         "should display two messages");
+
+    parent.destroy();
+});
+
+QUnit.module('LiveChat Tests', {});
+
+QUnit.test('define several livechat colors', async function (assert) {
+    assert.expect(2);
+
+    var parent = new Widget();
+
+    var livechat = new Livechat({
+        parent: parent,
+        data: {
+            id: 5,
+            message_unread_counter: 2,
+            name: 'myLivechat',
+            operator_pid: [1, "YourOperator"],
+        }
+    });
+
+    var messages = [];
+    for (var i = 0; i < 2; i++) {
+        messages.push(new Message(parent, {
+            id: i,
+            body: "<p>test" + i + "</p>"
+        }, {
+            default_username: 'defaultUser',
+            serverUrl: 'serverUrl',
+        }));
+    }
+
+    livechat.setMessages(messages);
+
+    var chatWindow = new ChatWindow(parent, livechat, {
+        headerBackgroundColor: 'yellow',
+        titleColor: 'green',
+    });
+    testUtils.mock.addMockEnvironment(parent, {debug:true});
+    await chatWindow.appendTo($('#qunit-fixture'));
+    chatWindow.render();
+    assert.strictEqual(chatWindow.$('.o_thread_window_header').css('background-color'), 'rgb(255, 255, 0)',
+        "header background color should be yellow");
+    assert.strictEqual(chatWindow.$('.o_thread_window_header').css('color'), 'rgb(0, 128, 0)',
+        "header text color should be green");
 
     parent.destroy();
 });
