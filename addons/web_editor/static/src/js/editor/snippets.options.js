480cc28f836dcc70d3be860473cfda5032fbb74f
Antoine Guenet <age@odoo.com>
2019-08-06 15:54:22 +0200

    replay 2a39ab0

--- addons/web_editor/static/src/js/editor/snippets.options.js
+++ addons/web_editor/static/src/js/editor/snippets.options.js
@@ -124,10 +124,10 @@ var SnippetOption = Widget.extend({
      *          that second case, the value is 'reset')
      *        - false if the option should be activated for good
      * @param {*} value - the class to activate ($opt.data('selectClass'))
-     * @param {jQuery} $opt - the related DOMElement option
+     * @param {jQuery} $li - the related DOMElement option
      */
-    selectClass: function (previewMode, value, $opt) {
-        var $group = $opt && $opt.closest('.dropdown-submenu');
+    selectClass: function (previewMode, value, $li) {
+        var $group = $li && $li.parents('.dropdown-submenu').last();
         if (!$group || !$group.length) {
             $group = this.$el;
         }
@@ -289,13 +289,29 @@ var SnippetOption = Widget.extend({
             })
             .addClass('active');
 
-        _processSelectClassElements(this.$el);
+        // Get submenus which are not inside submenus
+        var $submenus = this.$el.find('.dropdown-submenu')
+            .addBack('.dropdown-submenu')
+            .not('.dropdown-submenu .dropdown-submenu');
 
-        function _processSelectClassElements($el) {
+        // Add unique active class for each submenu active item
+        _.each($submenus, function (submenu) {
+            var $elements = _getSelectClassElements($(submenu));
+            _processSelectClassElements($elements);
+        });
+
+        // Add unique active class for out-of-submenu active item
+        var $externalElements = _getSelectClassElements(this.$el)
+            .not('.dropdown-submenu *, .dropdown-submenu');
+        _processSelectClassElements($externalElements);
+
+        function _getSelectClassElements($el) {
+            return $el.find('[data-select-class]')
+                .addBack('[data-select-class]');
+        }
+        function _processSelectClassElements($elements) {
             var maxNbClasses = -1;
-            $el.find('[data-select-class]')
-                .addBack('[data-select-class]')
-                .removeClass('active')
+            $elements.removeClass('active')
                 .filter(function () {
                     var className = $(this).data('selectClass');
                     var nbClasses = className ? className.split(' ').length : 0;
