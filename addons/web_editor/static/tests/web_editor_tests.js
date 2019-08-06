9e94d6bb45707d07e916a7ad9ff162478c3699c9
Antoine Guenet <age@odoo.com>
2019-08-06 15:21:45 +0200

    replay 1ced3bf

--- addons/web_editor/static/tests/web_editor_tests.js
+++ addons/web_editor/static/tests/web_editor_tests.js
@@ -109,7 +109,7 @@ QUnit.test('field html translatable', function (assert) {
         res_id: 1,
         mockRPC: function (route, args) {
             if (route === '/web/dataset/call_button' && args.method === 'translate_fields') {
-                assert.deepEqual(args.args, ['mass.mailing',1,'body',{}], "should call 'call_button' route");
+                assert.deepEqual(args.args, ['note.note', 1, 'body'], "should call 'call_button' route");
                 return $.when();
             }
             return this._super.apply(this, arguments);
