shaneebkhan/android-app

[IMP] mrp: log activity on parent manufacturing order.
 Before commit: - If there are multi level production order,
and user cancelled any one then,
there is no activity logged on related production order.

After this commit: - In multi level production order
(i.e. one production order creates another production order
to consume the raw material)
 if user cancelled raw material production order then,
exception will be generated on main production order,
so that user can easily get the idea that
raw material for this production order is not available.

task-1819526.

addons/mrp/wizard/change_production_qty.py
@@ -72,7 +72,7 @@ class ChangeProductionQty(models.TransientModel):
                 for operation in bom.routing_id.operation_ids:
                     operation_bom_qty[operation.id] = bom_data['qty']
             finished_moves_modification = self._update_finished_moves(production, production.product_qty - qty_produced, old_production_qty)
-            production._log_downside_manufactured_quantity(finished_moves_modification)
+            production._log_downside_manufactured_quantity(finished_moves_modification, cancel=False)
             moves = production.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
             moves._action_assign()
             for wo in production.workorder_ids:

yf956613/odoo-1

addons/mrp/wizard/change_production_qty.py
@@ -73,14 +73,13 @@ class ChangeProductionQty(models.TransientModel):
                             documents[key] = [value]
 
                 production._update_raw_move(line, line_data)
-
             production._log_manufacture_exception(documents)
             operation_bom_qty = {}
             for bom, bom_data in boms:
                 for operation in bom.routing_id.operation_ids:
                     operation_bom_qty[operation.id] = bom_data['qty']
             finished_moves_modification = self._update_finished_moves(production, production.product_qty - qty_produced, old_production_qty)
-            production._log_downside_manufactured_quantity(finished_moves_modification)
+            production._log_downside_manufactured_quantity(finished_moves_modification, cancel=True)
             moves = production.move_raw_ids.filtered(lambda x: x.state not in ('done', 'cancel'))
             moves._action_assign()
             for wo in production.workorder_ids: