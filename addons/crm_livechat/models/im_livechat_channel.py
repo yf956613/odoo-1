12fc56eedb3c35c319932f54bb687461f403235d
Priyanka Kakadiya <pka@odoo.com>
2019-08-20 19:02:01 +0530

    [IMP] crm_livechat: create new method

--- addons/crm_livechat/models/im_livechat_channel.py
+++ addons/crm_livechat/models/im_livechat_channel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, models
+from odoo import models
 
 
 class ImLivechatChannel(models.Model):
@@ -10,26 +10,8 @@ class ImLivechatChannel(models.Model):
     def _get_mail_channel(self, anonymous_name, previous_operator_id=None, user_id=None, country_id=None):
         self.ensure_one()
         res = super(ImLivechatChannel, self)._get_mail_channel(anonymous_name, previous_operator_id=previous_operator_id, user_id=user_id, country_id=country_id)
-
         if not res:
-            channel_partner_to_add = []
-            visitor_user = False
-            if user_id:
-                visitor_user = self.env['res.users'].browse(user_id)
-                if visitor_user and visitor_user.active:  # valid session user (not public)
-                    channel_partner_to_add.append((4, visitor_user.partner_id.id))
-
-            mail_channel = self.env["mail.channel"].with_context(mail_create_nosubscribe=False).sudo().create({
-                'channel_partner_ids': channel_partner_to_add,
-                'livechat_operator_id': False,
-                'livechat_channel_id': self.id,
-                'anonymous_name': False if user_id else anonymous_name,
-                'country_id': country_id,
-                'channel_type': 'livechat',
-                'name': visitor_user.name if visitor_user else anonymous_name,
-                'public': 'private',
-                'email_send': False,
-            })
+            mail_channel = self._create_mail_channel(None, user_id, anonymous_name, country_id)
             return mail_channel.sudo().channel_info()[0]
         return res
 
--- addons/im_livechat/models/im_livechat_channel.py
+++ addons/im_livechat/models/im_livechat_channel.py
@@ -104,6 +104,28 @@ class ImLivechatChannel(models.Model):
         self.ensure_one()
         return self.user_ids.filtered(lambda user: user.im_status == 'online')
 
+    def _create_mail_channel(self, operator_partner_id, user_id, anonymous_name, country_id=None, operator=None):
+        channel_partner_to_add = [(4, operator_partner_id)]
+        visitor_user = False
+        if user_id:
+            visitor_user = self.env['res.users'].browse(user_id)
+            if visitor_user and visitor_user.active:  # valid session user (not public)
+                channel_partner_to_add.append((4, visitor_user.partner_id.id))
+        channel_name = visitor_user.name if visitor_user else anonymous_name
+        if operator:
+            channel_name += ", %s" % operator.livechat_username or operator.name
+        return self.env["mail.channel"].with_context(mail_create_nosubscribe=False).sudo().create({
+            'channel_partner_ids': channel_partner_to_add,
+            'livechat_operator_id': operator_partner_id,
+            'livechat_channel_id': self.id,
+            'anonymous_name': False if user_id else anonymous_name,
+            'country_id': country_id,
+            'channel_type': 'livechat',
+            'name': channel_name,
+            'public': 'private',
+            'email_send': False,
+        })
+
     def _get_mail_channel(self, anonymous_name, previous_operator_id=None, user_id=None, country_id=None):
         """ Return a mail.channel given a livechat channel. It creates one with a connected operator, or return false otherwise
             :param anonymous_name : the name of the anonymous person of the channel
@@ -133,24 +155,8 @@ class ImLivechatChannel(models.Model):
 
         operator_partner_id = operator.partner_id.id
         # partner to add to the mail.channel
-        channel_partner_to_add = [(4, operator_partner_id)]
-        visitor_user = False
-        if user_id:
-            visitor_user = self.env['res.users'].browse(user_id)
-            if visitor_user and visitor_user.active:  # valid session user (not public)
-                channel_partner_to_add.append((4, visitor_user.partner_id.id))
         # create the session, and add the link with the given channel
-        mail_channel = self.env["mail.channel"].with_context(mail_create_nosubscribe=False).sudo().create({
-            'channel_partner_ids': channel_partner_to_add,
-            'livechat_operator_id': operator_partner_id,
-            'livechat_channel_id': self.id,
-            'anonymous_name': False if user_id else anonymous_name,
-            'country_id': country_id,
-            'channel_type': 'livechat',
-            'name': ', '.join([visitor_user.name if visitor_user else anonymous_name, operator.livechat_username if operator.livechat_username else operator.name]),
-            'public': 'private',
-            'email_send': False,
-        })
+        mail_channel = self._create_mail_channel(operator_partner_id, user_id, anonymous_name, country_id, operator)
         mail_channel._broadcast([operator_partner_id])
         return mail_channel.sudo().channel_info()[0]
 
