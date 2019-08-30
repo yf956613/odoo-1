997b033e19f29de0e1e86187e129a7ab323128d1
Jeremy Kersten <jke@odoo.com>
2019-08-30 10:22:29 +0200

    [IMP] base, http_routing, website: allow custom routing rule
    After this commit, you will be able (in technical mode) to update the url for
    the python controllers.
    
    Eg.
    You can now rename /shop in /garden and /shop/product/ in /garden/vegetable/
    
    Most of urls will be replaced at fly in the renderd qweb, with the function
    url_for but all old urls will keep available. So if you access url /shop you
    will be automatically redirected to /garden (308 Permanent Redirect).
    
    As for cdn and other post-process of att, the automatically replacement in the
    rendered qweb is only done when you will be not website editor. But the new
    dispatch of URL will be applied in all cases.
    
    For developper, since it is Permanent Redirect, donc forget to clear cache or
    open chrome debug tool (with option 'Disable cache while DevTools is Open) to
    see your lasts changes.

--- addons/website/controllers/main.py
+++ addons/website/controllers/main.py
@@ -4,7 +4,6 @@ import base64
 import datetime
 import json
 import os
-import re
 import logging
 import requests
 import werkzeug.utils
@@ -25,8 +24,6 @@ from odoo.addons.portal.controllers.web import Home
 
 logger = logging.getLogger(__name__)
 
-# Completely arbitrary limits
-MAX_IMAGE_WIDTH, MAX_IMAGE_HEIGHT = IMAGE_LIMITS = (1024, 768)
 LOC_PER_SITEMAP = 45000
 SITEMAP_CACHE_TIME = datetime.timedelta(hours=12)
 
