69035f0f4dbc3e2f6845e22cf09ab4c556f6f519
laa <laa@odoo.com>
2019-09-11 17:04:40 +0200

    [IMP]  mass_mailing :  Add dynamic placeholder
    Add a page with the dynamic placeholder generator just as it already exists on mail.templates.

--- addons/mass_mailing/models/mailing.py
+++ addons/mass_mailing/models/mailing.py
@@ -127,6 +127,64 @@ class MassMailing(models.Model):
     bounced_ratio = fields.Integer(compute="_compute_statistics", string='Bounced Ratio')
     next_departure = fields.Datetime(compute="_compute_next_departure", string='Scheduled date')
 
+    # Fake fields used to implement the placeholder assistant
+    model_object_field = fields.Many2one('ir.model.fields', string="Field",
+                                         help="Select target field from the related document model.\n"
+                                              "If it is a relationship field you will be able to select "
+                                              "a target field at the destination of the relationship.")
+    sub_object = fields.Many2one('ir.model', 'Sub-model', readonly=True,
+                                 help="When a relationship field is selected as first field, "
+                                      "this field shows the document model the relationship goes to.")
+    sub_model_object_field = fields.Many2one('ir.model.fields', 'Sub-field',
+                                             help="When a relationship field is selected as first field, "
+                                                  "this field lets you select the target field within the "
+                                                  "destination document model (sub-model).")
+    null_value = fields.Char('Default Value', help="Optional value to use if the target field is empty")
+    copyvalue = fields.Char('Placeholder Expression', help="Final placeholder expression, to be copy-pasted in the desired template field.")
+
+    @api.onchange('mailing_model_id')
+    def onchange_mailing_model_id(self):
+        # TDE CLEANME: should'nt it be a stored related ?
+        if self.mailing_model_id:
+            self.mailing_model_name = self.mailing_model_id.model
+        else:
+            self.mailing_model_name = False
+
+    def build_expression(self, field_name, sub_field_name, null_value):
+        """Returns a placeholder expression for use in a template field,
+        based on the values provided in the placeholder assistant.
+        :param field_name: main field name
+        :param sub_field_name: sub field name (M2O)
+        :param null_value: default value if the target value is empty
+        :return: final placeholder expression """
+        expression = ''
+        if field_name:
+            expression = "${object." + field_name
+            if sub_field_name:
+                expression += "." + sub_field_name
+            if null_value:
+                expression += " or '''%s'''" % null_value
+            expression += "}"
+        return expression
+
+    @api.onchange('model_object_field', 'sub_model_object_field', 'null_value')
+    def onchange_sub_model_object_value_field(self):
+        if self.model_object_field:
+            if self.model_object_field.ttype in ['many2one', 'one2many', 'many2many']:
+                mailing_model_name = self.env['ir.model']._get(self.model_object_field.relation)
+                if mailing_model_name:
+                    self.sub_object = mailing_model_name.id
+                    self.copyvalue = self.build_expression(self.model_object_field.name, self.sub_model_object_field and self.sub_model_object_field.name or False, self.null_value or False)
+            else:
+                self.sub_object = False
+                self.sub_model_object_field = False
+                self.copyvalue = self.build_expression(self.model_object_field.name, False, self.null_value or False)
+        else:
+            self.sub_object = False
+            self.copyvalue = False
+            self.sub_model_object_field = False
+            self.null_value = False
+            
     def _compute_total(self):
         for mass_mailing in self:
             mass_mailing.total = len(mass_mailing.sudo()._get_recipients())
