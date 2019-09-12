37c18ea68700da5445c2c74869604c8fa51d5455
William Henrotin <whe@odoo.com>
2019-09-12 15:38:45 +0200

    [FIX] stock: remove dummy field from depends_context
    This commit removes 'location' and 'warehouse' from the api.depends_context
    of _compute_quantities() as:
    
    1. They are not needed. They are never changed in a single transaction in the current code
    2. The ORM doens't support the fact that they are sometimes passed as list instead of integer

--- addons/stock/models/product.py
+++ addons/stock/models/product.py
@@ -91,7 +91,7 @@ class Product(models.Model):
     @api.depends('stock_move_ids.product_qty', 'stock_move_ids.state')
     @api.depends_context(
         'lot_id', 'owner_id', 'package_id', 'from_date', 'to_date',
-        'company_owned', 'location', 'warehouse', 'force_company',
+        'company_owned', 'force_company',
     )
     def _compute_quantities(self):
         products = self.filtered(lambda p: p.type != 'service')
@@ -611,7 +611,7 @@ class ProductTemplate(models.Model):
         'product_variant_ids.stock_move_ids.product_qty',
         'product_variant_ids.stock_move_ids.state',
     )
-    @api.depends_context('company_owned', 'location', 'warehouse', 'force_company')
+    @api.depends_context('company_owned', 'force_company')
     def _compute_quantities(self):
         res = self._compute_quantities_dict()
         for template in self:
